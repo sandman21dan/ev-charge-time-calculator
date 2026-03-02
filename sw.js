3fa6773054f50c196fb2bc910d41aec982338c8c
Daniel Perez <danielperezvl@gmail.com>
2026-03-02 11:57:39 +0000

    fix: update service worker to allow new changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ev-charge-time-calculator-v2';
+const CACHE_NAME = 'ev-charge-time-calculator-v3';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -11,12 +11,28 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
